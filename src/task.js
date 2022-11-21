e3a6ff4c94b241fa2363111ee1563d8c772c70ac
Ibrahim Qureshi <muhammadibrahim.alphasquad@gmail.com>
2022-11-21 17:14:19 +0500

    Fix grid display

--- src/task.js
+++ src/task.js
@@ -14,6 +14,31 @@ export default function Task() {
   const [gridArr, setGridArr] = useState();
   const [resetButtonVisible, setResetButtonVisible] = useState(false);
   const [tooltipVisible, setTooltipVisible] = useState(false);
+  const [inputFieldDisabled, setInputFieldDisabled] = useState(false);
+
+  // Get value from input field
+  const getInputValue = (event) => {
+    // let dup = Math.max(min, Math.min(max, Number(event.target.value)))
+    let dup = event.target.value;
+    // let dup = event.target.value
+    setUserValue(dup);
+
+    // Sets number of columns and rows for grid
+    document.documentElement.style.setProperty("--col-num", dup);
+    document.documentElement.style.setProperty("--row-num", dup);
+  };
+
+  function createGridValues(cols) {
+    for (let i = 0; i < cols; i++) {
+      let num = Math.floor(Math.random() * cols) + 1;
+      if (gridValues.includes(num)) {
+        i = i - 1;
+      } else {
+        gridValues.push(num);
+      }
+    }
+    setGridArr(gridValues);
+  }
 
   // Draggable Items on desktop
   const dragStart = (e, idx) => {
@@ -40,14 +65,42 @@ export default function Task() {
     if (JSON.stringify(copyListItems) === JSON.stringify(sorted)) {
       setTimeout(alert("Task completed successfully!"), 20000);
     }
-
-    function sortGridArr(arr) {
-      arr.sort(function (a, b) {
-        return a - b;
-      });
-      return arr;
-    }
   };
+
+  function sortGridArr(arr) {
+    arr.sort(function (a, b) {
+      return a - b;
+    });
+    return arr;
+  }
+
+  function mobDrad() {
+    // find the element that you want to drag.
+    var box = document.getElementById("mob-drag");
+
+    /* listen to the touchmove event,
+    every time it fires, grab the location
+    of touch and assign it to box */
+
+    box.addEventListener("touchmove", function (e) {
+      // grab the location of touch
+      var touchLocation = e.targetTouches[0];
+
+      // assign box new coordinates based on the touch.
+      box.style.left = touchLocation.pageX + "px";
+      box.style.top = touchLocation.pageY + "px";
+    });
+
+    /* record the position of the touch
+  when released using touchend event.
+  This will be the drop position. */
+
+    box.addEventListener("touchend", function (e) {
+      // current box position.
+      var x = parseInt(box.style.left);
+      var y = parseInt(box.style.top);
+    });
+  }
 
   return (
     <div className="w-full h-full xxl:h-[1555px] xxl:pb-[169px]">
@@ -68,16 +121,13 @@ export default function Task() {
               <input
                 value={userValue}
                 type="number"
-                min="10"
-                max="30"
-                onChange={(e) => {
-                  setUserValue(e.target.value);
-                }}
+                onChange={getInputValue}
                 onFocus={() => {
                   setTooltipVisible(false);
                 }}
                 placeholder="Enter a number!"
                 className="p-3 sm:p-4 border border-gray-200 rounded-[4px] w-full sm:w-[340px] sm:mr-8 mb-4 sm:mb-0"
+                disabled={inputFieldDisabled}
               />
 
               {/* Custom Tooltip */}
@@ -86,28 +136,21 @@ export default function Task() {
                   (tooltipVisible && `opacity-100`) || `opacity-0`
                 } transition-opacity duration-300 `}
               >
-                <p>Please enter a value between 1 and 30!</p>
+                <p>Please enter a value from 1 to 6!</p>
               </div>
 
               {/* Submit Button*/}
               <button
                 type="submit"
                 onClick={() => {
-                  if (userValue > 30) {
+                  if (userValue > 6) {
                     setTooltipVisible(true);
                   } else if (userValue > 0) {
                     setTooltipVisible(true);
                     setTooltipVisible(false);
-                    for (let i = 0; i < userValue; i++) {
-                      let num = Math.floor(Math.random() * userValue) + 1;
-                      if (gridValues.includes(num)) {
-                        i = i - 1;
-                      } else {
-                        gridValues.push(num);
-                      }
-                    }
-                    setGridArr(gridValues);
+                    createGridValues(userValue * userValue);
                     setResetButtonVisible(true);
+                    setInputFieldDisabled(true);
                   }
                 }}
                 className="bg-blue-600 border border-transparent text-white w-full rounded
@@ -124,6 +167,7 @@ export default function Task() {
                     setGridArr();
                     setUserValue("");
                     setResetButtonVisible(false);
+                    setInputFieldDisabled(false);
                   }}
                   // className='sm:hidden text-blue-600 border border-blue-600 px-12 py-1 rounded-2xl mr-2 absolute right-0 self-end w-full max-w-[130px] xs375:max-w-[150px] xs375:py-1.5  xs425:max-w-[180px] xs425:py-2'>
                   className="sm:hidden text-blue-600 border border-blue-600 py-[14px] px-8 text-sm leading-[14px] rounded w-full mt-2"
@@ -141,6 +185,7 @@ export default function Task() {
                     setGridArr();
                     setUserValue("");
                     setResetButtonVisible(false);
+                    setInputFieldDisabled(false);
                   }}
                   className="hidden sm:block text-blue-600 border border-blue-600 rounded-[4px] px-8 py-4 "
                 >
@@ -182,12 +227,18 @@ export default function Task() {
 
           {/* Grid container with fixed width and adjustable height */}
           <div className="bg-orange-100 border border-orange-600 min-h-[250px] md:min-h-[350px] xxl:h-[744px] xl:w-[1080] xxl:max-w-[1535px] rounded-3xl p-6 xxl:p-12 mx-auto">
-            <div className="flex flex-wrap gap-x-2 xxl:gap-x-8 gap-y-2 xxl:gap-y-4 mx-auto w-full">
+            <div
+              style={{ gridTemplateColumns: `repeat(${userValue}, 1fr` }}
+              className={`grid grid-cols-${userValue} gap-x-2 xxl:gap-x-8 gap-y-2 xxl:gap-y-4 mx-auto w-full`}
+              //   className="grid flex flex-wrap gap-x-2 xxl:gap-x-8 gap-y-2 xxl:gap-y-4 mx-auto w-full"
+            >
               {gridArr?.map((item, idx) => {
                 return (
                   <div
+                    id="mob-drag"
                     key={idx * 1000 * Math.random()}
-                    className={`flex items-center justify-center min-w-[50px] min-h-[50px] xs320:w-[70px] xs320:h-[70px] xs375:w-[89px] xs375:h-[87px] xs425:w-[75px] xs425:h-[75px] sm:w-[125px] sm:h-[125px] md:w-[145px] md:h-[150px] lg:w-[171px] lg:h-[150px] xxl:w-[212px] xxl:h-[150px] text-white text-lg xxl:text-[32px] leading-8 text-center rounded-2xl 
+                    // className={`flex items-center justify-center min-w-[50px] min-h-[50px] xs320:w-[70px] xs320:h-[70px] xs375:w-[89px] xs375:h-[87px] xs425:w-[75px] xs425:h-[75px] sm:w-[125px] sm:h-[125px] md:w-[145px] md:h-[150px] lg:w-[171px] lg:h-[150px] xxl:w-[212px] xxl:h-[150px] text-white text-lg xxl:text-[32px] leading-8 text-center rounded-2xl
+                    className={`flex items-center justify-center text-white text-lg xxl:text-[32px] leading-8 text-center rounded-2xl min-h-[50px]  xs320:h-[70px] xs375:h-[87px] xs425:h-[75px] sm:h-[125px] md:h-[150px] lg:h-[150px] xxl:h-[150px]
                                         ${
                                           (item > 2 &&
                                             item % 2 !== 0 &&
@@ -206,6 +257,8 @@ export default function Task() {
                     onDragStart={(e) => dragStart(e, idx)}
                     onDragEnter={(e) => dragEnter(e, idx)}
                     onDragEnd={drop}
+                    // Touch functions
+                    onTouchStart={(e) => mobDrad(e, idx)}
                     draggable
                   >
                     {item}
